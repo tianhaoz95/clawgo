09207b132b0a97dea2fb69a3d29725e5aa1f5ae3
Tianhao Zhou <tianhaozhou95@icloud.com>
2026-03-21 17:48:23 -0700

    fb init

--- clawgo/cyberclaw/lib/main.dart
+++ clawgo/cyberclaw/lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:cyberclaw/src/core/router.dart';
 import 'package:cyberclaw/src/core/theme.dart';
 import 'package:cyberclaw/src/rust/frb_generated.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
+import 'firebase_options.dart';
+
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await RustLib.init();
   runApp(const MyApp());
 }
